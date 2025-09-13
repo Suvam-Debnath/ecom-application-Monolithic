4119e025b314ae8e8e538fd9c443c0b9cc1e8ebb
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-13 11:35:25 +0530

    added ResponseEntity and customised status codes

--- src/main/java/com/app/ecom/UserController.java
+++ src/main/java/com/app/ecom/UserController.java
@@ -1,6 +1,8 @@
 package com.app.ecom;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,18 +15,21 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers(){
-        return userService.fetchAllUsers();
+    public ResponseEntity<List<User>> getAllUsers(){
+        //return ResponseEntity.ok(userService.fetchAllUsers());
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/api/users/{id}")
-    public User getUser(@PathVariable Long id){
-        return userService.fetchUser(id);
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        User user = userService.fetchUser(id);
+        if(user==null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping("/api/users")
-    public String createUser(@RequestBody User user){
+    public ResponseEntity<String> createUser(@RequestBody User user){
         userService.addUser(user);
-        return "User added successfully";
+        return ResponseEntity.ok("User added successfully");
     }
 }
