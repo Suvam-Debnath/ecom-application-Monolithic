9b91fa4697400c24f1c2e7c1002e648825740018
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-13 17:25:15 +0530

    usin RequestMapping annotation

--- src/main/java/com/app/ecom/UserController.java
+++ src/main/java/com/app/ecom/UserController.java
@@ -9,31 +9,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/api/users")
+    @GetMapping
+    //@RequestMapping(value = "/api/users", method = RequestMethod.GET)
     public ResponseEntity<List<User>> getAllUsers(){
         //return ResponseEntity.ok(userService.fetchAllUsers());
         return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id){
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(()->ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user){
         userService.addUser(user);
         return ResponseEntity.ok("User added successfully");
     }
 
-    @PutMapping("/api/users/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@PathVariable Long id,@RequestBody User updatedUser){
         boolean updated = userService.updateUser(id,updatedUser);
         if (updated) return ResponseEntity.ok("User updated successfully");
